52a854c6558a8e2a8d43f5bf16f7657d132c4755
kwlucas <lucaswkyle@gmail.com>
2022-07-28 13:01:26 -0600

    Minor psudo codr

---
+++ assets/str-eval.py
@@ -0,0 +1,7 @@
+# identify parts of speech in a sentence
+# use sentence structure with a CART algorithm to classify sentence structure
+# with the sentence structure identified can now classify the parts of speech of each word
+
+# rate the moods/tones and complexity of words/phrases
+
+
